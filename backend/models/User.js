b76ee6f85db499f05f21107929f1111ce70b76d0
AbhishekSaroch123 <abhishek@prodsmiths>
2024-04-14 00:48:21 +0530

    changed some files in backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,25 +28,4 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
-async function sendLoginMail(email) {
-  try {
-    const mailResponse = await mailSender(
-      email,
-      "Login Successful on Bloggerly"
-    );
-    console.log("E-mail sent successfully", mailResponse.response);
-  } catch (error) {
-    console.log("Error Occored while sending mail", error);
-    throw error;
-  }
-}
 
-userSchema.pre("save", async function (next) {
-    console.log("New document saved to database");
-  if (this.isNew) {
-    await sendVerificationEmail(this.email);
-  }
-  next();
-});
-
-module.exports = mongoose.model("user", userSchema);
